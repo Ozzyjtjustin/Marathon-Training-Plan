4f6b4ee226c962b8f218a37f4a40d8ee8203f8d7
Justin Oswald <justin@meshify.com>
2019-06-04 10:53:02 -0500

    fusion running commit

--- src/App.js
+++ src/App.js
@@ -17,7 +17,7 @@ class App extends Component {
             target="_blank"
             rel="noopener noreferrer"
           >
-            Learn React
+This will be my future fusion running app
           </a>
         </header>
       </div>
