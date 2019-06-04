08b68e2ed9779dffd2399ae1fec1b39c2c694ca8
Justin Oswald <justin@meshify.com>
2019-06-04 11:01:20 -0500

    logistics

--- src/App.js
+++ src/App.js
@@ -27,3 +27,33 @@ This will be my future fusion running app
 }
 
 export default App;
+
+import React, { Component } from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+class App extends Component {
+    render() {
+        return (
+            <div className="App">
+                <header className="App-header">
+                    <img src={logo} className="App-logo" alt="logo" />
+                    <h1>
+                        Fusion Running
+                    </h1>
+                    <a
+                        className="App-link"
+                        href="https://reactjs.org"
+                        target="_blank"
+                        rel="noopener noreferrer"
+                    >
+                        Train at your pace
+                        This will be my future fusion running app
+                    </a>
+                </header>
+            </div>
+        );
+    }
+}
+
+export default App;
